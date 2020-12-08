834dd55cc4efcb2110d41241b9642f5245ba854c
eric.wonbin.sang <eric.wonbin.sang@gmail.com>
2020-12-08 10:09:04 -0500

    Added jonathan files.

---
+++ js_dir/script_jonathan.js
@@ -0,0 +1,76 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+
+
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function incrementValue2()
+{
+    var value1=0;
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value1;
+}
+function incrementValue3()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function incrementValue4()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function incrementValue5()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+function incrementValue6()
+{
+    var value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
--- js_dir/script_will.js
+++ js_dir/script_will.js
@@ -11,20 +11,12 @@ function likeImage(imgNum)
     var likes = document.getElementsByClassName("numlikes");
     var buttons = document.getElementsByClassName("like_button");
     
-    
-    if (buttons[imgNum].src.includes("images/general/liked_pc.png")){
-        
+    if (buttons[imgNum].src.includes("images/general/liked_pc.png")) {
         likes[imgNum].innerHTML = parseInt(likes[imgNum].textContent) - 1;
         buttons[imgNum].src = "images/general/unliked_pc.png";
-        
     }
-    
-    else
-    {
-        
+    else {
         likes[imgNum].innerHTML = parseInt(likes[imgNum].textContent) + 1;
         buttons[imgNum].src = "images/general/liked_pc.png";
-        
     }
-    
 }
